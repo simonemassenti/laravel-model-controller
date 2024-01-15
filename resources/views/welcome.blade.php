ecf4ce9b252b24e78d47ecd1f98dfaf5552bafb8
simonemassenti <s.massenti96@gmail.com>
2024-01-15 14:51:58 +0100

    add: movies

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Welcome to laravel</h1>
+    <h1 class="text-center my-4">Movies</h1>
+
+    <div class="container my-3">
+        <div class="row">
+            <div class="col d-flex flex-wrap gap-4 justify-content-around">
+                @foreach ($movies as $movie)
+                    <div class="movie-card">
+                        <div>
+                            <img src="{{ $movie->image }}" alt="{{ $movie->title }}">
+                        </div>
+                        <h2 class="text-center px-3">{{ $movie->title }}</h2>
+                        <hr>
+                        <div class="px-2">
+                            <h6>Titolo Originale: {{ $movie->original_title }}</h6>
+                            <h6>Nazione: {{ $movie->nationality }}</h6>
+                            <h6>Data di uscita: {{ $movie->date }}</h6>
+                            <h6>Voto: {{ $movie->vote }}</h6>
+                        </div>
+
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
 @endsection
